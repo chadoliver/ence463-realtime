6603456a46a660f05be7cd41d85995ddb238c954
Chad Oliver <hello@chadoliver.co.nz>
2013-08-09 15:40:36 +1200

    Created a function to write registers using I2C. Can't figure out how to do register reads. Committing before I attempt to use a third-party i2c library.

--- Src/types.h
+++ Src/types.h
@@ -8,7 +8,21 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
+#define TRUE 	(1)
+#define FALSE 	(0)
+
 typedef void (*function_ptr)( void * );
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed int int16_t;
+typedef unsigned int 	uint16_t;
+typedef signed long int int32_t;
+typedef unsigned long int uint32_t;
+typedef signed long long int int64_t;
+typedef unsigned long long int uint64_t;
+
+typedef uint8_t reg_data;
 
 typedef enum {
 	ACCEL_X,
@@ -25,14 +39,14 @@ typedef struct {
 } ScreenMessage;
 
 typedef struct {
-	long x;
-	long y;
-	long z;
-} Vector3;
+	uint16_t x;
+	uint16_t y;
+	uint16_t z;
+} vector3;
 
 typedef struct {
-	Vector3 accel;
-	Vector3 gyro;
+	vector3 accel;
+	vector3 gyro;
 } MeasurementState;
 
 #endif /* TYPES_H_ */
